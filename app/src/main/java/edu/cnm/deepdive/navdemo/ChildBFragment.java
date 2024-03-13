f805f2c995369f5e4c98df85cfed3e7119e0f7ae
DanielleScott <dmyers28@cnm.edu>
2024-03-13 16:57:52 -0600

    cleaned up code and implemented a button navigating to fragment c from fragment b

--- app/src/main/java/edu/cnm/deepdive/navdemo/ChildBFragment.java
+++ app/src/main/java/edu/cnm/deepdive/navdemo/ChildBFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import edu.cnm.deepdive.navdemo.databinding.FragmentChildBBinding;
 
 
@@ -13,7 +16,7 @@ public class ChildBFragment extends Fragment {
   private FragmentChildBBinding binding;
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+  public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     binding = FragmentChildBBinding.inflate(inflater, container, false);
     String content = ChildBFragmentArgs.fromBundle(getArguments()).getContent();
@@ -21,4 +24,10 @@ public class ChildBFragment extends Fragment {
     return binding.getRoot();
   }
 
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    binding.navigateC.setOnClickListener((button) ->
+        Navigation.findNavController(button).navigate(ChildBFragmentDirections.navigateToC()));
+  }
 }
